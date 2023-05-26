0902c68abde47deb0c07dd9892cb2dab832169a6
Gabriel M <gabriel.marchese2@gmail.com>
2023-05-26 16:40:07 -0300

    Teste Pronto

--- productStructure.js
+++ productStructure.js
@@ -1,18 +1,30 @@
-const products = [
-  "preto-PP",
-  "preto-M",
-  "preto-G",
-  "preto-GG",
-  "preto-GG",
-  "branco-PP",
-  "branco-G",
-  "vermelho-M",
-  "azul-XG",
-  "azul-XG",
-  "azul-XG",
-  "azul-P"
-]
+function productStructure() {
+  const result = {};
 
-module.exports = () => {
-	// Seu código vai aqui!
+  const products = [
+    "preto-PP",
+    "preto-M",
+    "preto-G",
+    "preto-GG",
+    "preto-GG",
+    "branco-PP",
+    "branco-G",
+    "vermelho-M",
+    "azul-XG",
+    "azul-XG",
+    "azul-XG",
+    "azul-P"
+  ];
+
+  products.forEach((item) => {
+    const [Cor, Tamanho] = item.split('-');
+
+    result[Cor] = result[Cor] || {};
+    result[Cor][Tamanho] = (result[Cor][Tamanho] || 0) + 1;
+  });
+
+  return result;
 }
+
+module.exports = productStructure;
+
